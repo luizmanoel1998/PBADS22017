02ef605af2f120f646d5b1c60a732c681b26690e
Luiz Manoel <manoelmattosmattos@outlook.com>
2017-09-25 20:44:28 -0400

    Atualização

---
+++ Prova01/Models/Automovel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prova01.Models
+{
+    public class Automovel
+    {
+        Automovel = int 
+
+    }
+}
---
+++ Prova01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Prova01.Startup))]
+namespace Prova01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
